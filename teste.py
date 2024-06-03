a77a0c9916b6d2deb6520eb16a6475e3a31c560d
jadna <jadna.ac@gmail.com>
2024-06-03 17:23:47 +0100

    conflito

--- teste.py
+++ teste.py
@@ -20,7 +20,3 @@ dataset = "scifact"
 url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
 out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
 data_path = util.download_and_unzip(url, out_dir)
-<<<<<<< HEAD
-=======
-
->>>>>>> c0efa86 (conflito)
