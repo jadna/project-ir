cf45fd29b055eb48f23f011ecb28cc28b1b4a25e
jadna <jadna.ac@gmail.com>
2024-06-03 18:16:35 +0100

    bm25 salvando

--- benchmark-bm25.py
+++ benchmark-bm25.py
@@ -20,7 +20,7 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 #### /print debug information to stdout
 
 #### Download dbpedia-entity.zip dataset and unzip the dataset
-dataset = "dbpedia-entity"
+dataset = "scifact"
 url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
 out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
 data_path = util.download_and_unzip(url, out_dir)
---
+++ project-bm25.py
@@ -0,0 +1,104 @@
+from beir import util, LoggingHandler
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.lexical import BM25Search as BM25
+import pathlib, os
+import datetime
+import logging
+import random
+import pandas as pd
+
+random.seed(42)
+
+# Elasticsearch
+hostname = "localhost" #localhost
+index_name = "hotpotqa" # scifact
+
+
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+
+
+url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(index_name)
+out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "datasets")
+data_path = util.download_and_unzip(url, out_dir)
+
+# Forneçe o caminho de dados onde o dataset foi baixado, descompactado e carrega os dados
+corpus, queries, qrels = GenericDataLoader(data_path).load(split="test")
+corpus_ids, query_ids = list(corpus), list(queries)
+
+# Amostra aleatória dos pares do Corpus Original
+# Incluindo primeiramente todos os documentos relevantes, ou seja, presentes em qrels
+corpus_set = set()
+for query_id in qrels:
+    corpus_set.update(list(qrels[query_id].keys()))
+corpus_new = {corpus_id: corpus[corpus_id] for corpus_id in corpus_set}
+
+# Remove k documentos relevantes já vistos e amostras de documentos aleatoriamente
+remaining_corpus = list(set(corpus_ids) - corpus_set)
+sample = 100000 - len(corpus_set)
+
+for corpus_id in random.sample(remaining_corpus, sample):
+    corpus_new[corpus_id] = corpus[corpus_id]
+
+model = BM25(index_name=index_name, hostname=hostname)
+bm25 = EvaluateRetrieval(model)
+
+# Recuperar resultados densos (o formato dos resultados é idêntico ao qrels)
+results = bm25.retrieve(corpus, queries)
+
+# Indexa as passagens no índice separadamente
+bm25.retriever.index(corpus_new)
+
+
+# Avaliação da IR usando NDCG@k, MAP@K, RECALL@K e PRECISION@K
+logging.info("Retriever evaluation for k in: {}".format(bm25.k_values)) 
+ndcg, _map, recall, precision = bm25.evaluate(qrels, results, bm25.k_values)
+
+# Criando um DataFrame com os dados
+data = {
+    'ndcg': [ndcg[k] for k in ndcg],
+    'map': [_map[k] for k in _map],
+    'recall': [recall[k] for k in recall],
+    'precision': [precision[k] for k in precision]
+}
+
+df = pd.DataFrame(data, index=['@{}'.format(k) for k in bm25.k_values])
+# df = pd.DataFrame(data, index=False)
+
+# xibir o DataFrame
+# print(df)
+ 
+# Retrieval Exemplo
+query_id, scores_dict = random.choice(list(results.items()))
+logging.info("Query : %s\n" % queries[query_id])
+
+scores = sorted(scores_dict.items(), key=lambda item: item[1], reverse=True)
+for rank in range(10):
+    doc_id = scores[rank][0]
+    logging.info("Doc %d: %s [%s] - %s\n" % (rank+1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
+
+# Salvando os tempos do benchmark
+time_taken_all = {}
+
+for query_id in query_ids:
+    query = queries[query_id]
+    
+    # Medida do tempo para recuperar os 10 principais documentos BM25 usando latência de consulta única
+    start = datetime.datetime.now()
+    results = bm25.retriever.es.lexical_search(text=query, top_hits=10) 
+    end = datetime.datetime.now()
+    
+    # Medição do tempo gasto em milissegundos (ms)
+    time_taken = (end - start)
+    time_taken = time_taken.total_seconds() * 1000
+    time_taken_all[query_id] = time_taken
+    logging.info("{}: {} {:.2f}ms".format(query_id, query, time_taken))
+
+
+time_taken = list(time_taken_all.values())
+logging.info("Average time taken: {:.2f}ms".format(sum(time_taken)/len(time_taken_all)))
+
+df.to_csv('bm25_'+index_name+'.csv')
